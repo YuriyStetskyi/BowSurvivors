27813710b8997f1e833b3f7600613109ffab3a51
Yuriy Stetskyi <yuriystetskyi@gmail.com>
2025-08-18 13:11:30 +0300

    GAS Gameplay Effects #2. Added niagaras for pickups. Added BWSEffectActor and set it up to apply gameplay effect

---
+++ Source/BowSurvivors/Actors/BWSEffectActor.cpp
@@ -0,0 +1,46 @@
+// Copyright (c) 2025 Yuriy "Steyur" Stetskyi - MIT License.
+
+
+#include "Actors/BWSEffectActor.h"
+#include "AbilitySystemBlueprintLibrary.h"
+#include "AbilitySystemComponent.h"
+#include "AbilitySystemInterface.h"
+
+// Sets default values
+ABWSEffectActor::ABWSEffectActor()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+    SetRootComponent(CreateDefaultSubobject<USceneComponent>("SceneRoot"));
+}
+
+// Called when the game starts or when spawned
+void ABWSEffectActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABWSEffectActor::ApplyEffectToTarget(AActor* const Target, TSubclassOf<UGameplayEffect> GameplayEffectClass)
+{
+    UAbilitySystemComponent* const TargetASC = UAbilitySystemBlueprintLibrary::GetAbilitySystemComponent(Target);
+    if (!TargetASC) return;
+
+    FGameplayEffectContextHandle EffectContextHandle = TargetASC->MakeEffectContext();
+    EffectContextHandle.AddSourceObject(this);
+
+    FGameplayEffectSpecHandle EffectSpecHandle = TargetASC->MakeOutgoingSpec(GameplayEffectClass, 1, EffectContextHandle);
+
+
+    TargetASC->ApplyGameplayEffectSpecToSelf(*EffectSpecHandle.Data.Get());
+         
+}
+
+// Called every frame
+void ABWSEffectActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/BowSurvivors/Actors/BWSEffectActor.h
@@ -0,0 +1,34 @@
+// Copyright (c) 2025 Yuriy "Steyur" Stetskyi - MIT License.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BWSEffectActor.generated.h"
+
+class UGameplayEffect;
+
+UCLASS()
+class BOWSURVIVORS_API ABWSEffectActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABWSEffectActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+    UPROPERTY(EditAnywhere, Category = "Applied Effects")
+    TSubclassOf<UGameplayEffect> InstantGameplayEffectClass;
+
+    UFUNCTION(BlueprintCallable)
+    void ApplyEffectToTarget(AActor* const Target, TSubclassOf<UGameplayEffect> GameplayEffectClass);
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};

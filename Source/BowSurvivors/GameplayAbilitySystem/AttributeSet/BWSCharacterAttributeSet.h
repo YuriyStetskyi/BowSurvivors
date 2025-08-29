d75efe9047a6f894e3b79fed0f109d072ad8a235
Yuriy Stetskyi <yuriystetskyi@gmail.com>
2025-08-29 20:49:34 +0300

    GAS Attributes #2. Added separate attribute sets for weapon and character

---
+++ Source/BowSurvivors/GameplayAbilitySystem/AttributeSet/BWSCharacterAttributeSet.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2025 Yuriy "Steyur" Stetskyi - MIT License.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayAbilitySystem/BWSAttributeSet.h"
+#include "BWSCharacterAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOWSURVIVORS_API UBWSCharacterAttributeSet : public UBWSAttributeSet
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/BowSurvivors/GameplayAbilitySystem/AttributeSet/BWSWeaponAttributeSet.h
@@ -0,0 +1,17 @@
+// Copyright (c) 2025 Yuriy "Steyur" Stetskyi - MIT License.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayAbilitySystem/BWSAttributeSet.h"
+#include "BWSWeaponAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOWSURVIVORS_API UBWSWeaponAttributeSet : public UBWSAttributeSet
+{
+	GENERATED_BODY()
+	
+};

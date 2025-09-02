4d19e504406ead1de5dd401126573c2b7f6188d4
Yuriy Stetskyi <yuriystetskyi@gmail.com>
2025-09-02 22:12:05 +0300

    GAS #6. Added all attributes to weapons

--- Source/BowSurvivors/GameplayAbilitySystem/AttributeSet/BWSWeaponAttributeSet.cpp
+++ Source/BowSurvivors/GameplayAbilitySystem/AttributeSet/BWSWeaponAttributeSet.cpp
@@ -2,4 +2,23 @@
 
 
 #include "GameplayAbilitySystem/AttributeSet/BWSWeaponAttributeSet.h"
+#include "GameplayEffectExtension.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemBlueprintLibrary.h"
 
+UBWSWeaponAttributeSet::UBWSWeaponAttributeSet(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
+{
+}
+
+void UBWSWeaponAttributeSet::PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue)
+{
+    Super::PreAttributeChange(Attribute, NewValue);
+
+}
+
+void UBWSWeaponAttributeSet::PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data)
+{
+    Super::PostGameplayEffectExecute(Data);
+
+}
--- Source/BowSurvivors/GameplayAbilitySystem/AttributeSet/BWSWeaponAttributeSet.h
+++ Source/BowSurvivors/GameplayAbilitySystem/AttributeSet/BWSWeaponAttributeSet.h
@@ -14,4 +14,64 @@ class BOWSURVIVORS_API UBWSWeaponAttributeSet : public UBWSAttributeSet
 {
 	GENERATED_BODY()
 	
+public:
+    UBWSWeaponAttributeSet(const FObjectInitializer& ObjectInitializer);
+
+    virtual void PreAttributeChange(const FGameplayAttribute& Attribute, float& NewValue) override;
+    virtual void PostGameplayEffectExecute(const FGameplayEffectModCallbackData& Data) override;
+
+    /*
+     *   Core Weapon Attributes
+     */
+
+    /* Amount of health character should lose when damaged by this weapons projectile */
+    UPROPERTY(BlueprintReadOnly, Category = "Attributes | Main | DPS")
+    FGameplayAttributeData Damage;
+    ATTRIBUTE_ACCESSORS(UBWSWeaponAttributeSet, Damage);
+
+    /* Describes how many times per second weapon should fire */
+    UPROPERTY(BlueprintReadOnly, Category = "Attributes | Main | DPS")
+    FGameplayAttributeData FireRate;
+    ATTRIBUTE_ACCESSORS(UBWSWeaponAttributeSet, FireRate);
+
+    /* Amount of projectiles shot when Attack() called in a cone */
+    UPROPERTY(BlueprintReadOnly, Category = "Attributes | Main | DPS")
+    FGameplayAttributeData ProjectilesCount;
+    ATTRIBUTE_ACCESSORS(UBWSWeaponAttributeSet, ProjectilesCount);
+
+    /* Distance projectile should travel before disappearing / falling to ground*/
+    UPROPERTY(BlueprintReadOnly, Category = "Attributes | Main | Utility")
+    FGameplayAttributeData Range;
+    ATTRIBUTE_ACCESSORS(UBWSWeaponAttributeSet, Range);
+
+    /* Speed at which projectile travels */
+    UPROPERTY(BlueprintReadOnly, Category = "Attributes | Main | Utility")
+    FGameplayAttributeData Speed;
+    ATTRIBUTE_ACCESSORS(UBWSWeaponAttributeSet, Speed);
+
+    /* Number by which default projectile scale is multiplied at creation to increase its size */
+    UPROPERTY(BlueprintReadOnly, Category = "Attributes | Main | Utility")
+    FGameplayAttributeData Size;
+    ATTRIBUTE_ACCESSORS(UBWSWeaponAttributeSet, Size);
+
+    /*
+     *   Derived Weapon Attributes
+     */
+
+    /* Amount of Damage bow should deal per second (multiple projectiles might make calculation inconsistent) */
+    UPROPERTY(BlueprintReadOnly, Category = "Attributes | Derived")
+    FGameplayAttributeData DPS;
+    ATTRIBUTE_ACCESSORS(UBWSWeaponAttributeSet, DPS);
+
+    /* Represents how easy it is to hit with projectiles created with weapon */
+    UPROPERTY(BlueprintReadOnly, Category = "Attributes | Derived")
+    FGameplayAttributeData UtilityScore;
+    ATTRIBUTE_ACCESSORS(UBWSWeaponAttributeSet, UtilityScore);
+
+    /* Represents how effective weapon is at eliminating enemies */
+    UPROPERTY(BlueprintReadOnly, Category = "Attributes | Main | Utility")
+    FGameplayAttributeData WeaponScore;
+    ATTRIBUTE_ACCESSORS(UBWSWeaponAttributeSet, WeaponScore);
+
+
 };

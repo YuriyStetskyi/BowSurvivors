9e8a4b0ae632de93f791b5e146af5cd3d0019f82
Yuriy Stetskyi <yuriystetskyi@gmail.com>
2025-09-25 18:01:44 +0300

    GAS WeaponUI #13. Attribute Info Data Asset created

--- Source/BowSurvivors/GameplayAbilitySystem/Data/BWSWeaponAttributeInfo.cpp
+++ Source/BowSurvivors/GameplayAbilitySystem/Data/BWSWeaponAttributeInfo.cpp
@@ -3,3 +3,28 @@
 
 #include "GameplayAbilitySystem/Data/BWSWeaponAttributeInfo.h"
 
+FWeaponAttributeInfo::FWeaponAttributeInfo()
+    : AttributeTag(FGameplayTag())
+    , AttributeName(FText())
+    , AttributeDescription(FText())
+    , AttributeValue(0.0f)
+{
+}
+
+FWeaponAttributeInfo UBWSWeaponAttributeInfo::FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound) const
+{
+    for (const FWeaponAttributeInfo& Info : AttributeInformation)
+    {
+        if (Info.AttributeTag == AttributeTag)
+        {
+            return Info;
+        }
+    }
+
+    if (bLogNotFound)
+    {
+        UE_LOG(LogTemp, Error, TEXT("Can't find Info for AttributeTag [%s] on AttributeInfo [%s]. "), *AttributeTag.ToString(), *GetNameSafe(this));
+    }
+
+    return FWeaponAttributeInfo();
+}
--- Source/BowSurvivors/GameplayAbilitySystem/Data/BWSWeaponAttributeInfo.h
+++ Source/BowSurvivors/GameplayAbilitySystem/Data/BWSWeaponAttributeInfo.h
@@ -4,7 +4,29 @@
 
 #include "CoreMinimal.h"
 #include "Engine/DataAsset.h"
+#include "GameplayTagContainer.h"
 #include "BWSWeaponAttributeInfo.generated.h"
+
+USTRUCT(BlueprintType)
+struct FWeaponAttributeInfo
+{
+    GENERATED_BODY()
+
+    FWeaponAttributeInfo();
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    FGameplayTag AttributeTag;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    FText AttributeName;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    FText AttributeDescription;
+
+    UPROPERTY(BlueprintReadOnly)
+    float AttributeValue;
+
+};
 
 /**
  * 
@@ -14,4 +36,11 @@ class BOWSURVIVORS_API UBWSWeaponAttributeInfo : public UDataAsset
 {
 	GENERATED_BODY()
 	
+public:
+
+    FWeaponAttributeInfo FindAttributeInfoForTag(const FGameplayTag& AttributeTag, bool bLogNotFound = false) const;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+    TArray<FWeaponAttributeInfo> AttributeInformation;
+
 };

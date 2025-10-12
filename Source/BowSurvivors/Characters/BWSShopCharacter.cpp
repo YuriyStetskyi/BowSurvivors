39b6023baad6d7bbfe9143f30d2f39984eeebcf7
Yuriy Stetskyi <yuriystetskyi@gmail.com>
2025-10-13 02:10:23 +0300

    Shopkeeper #3. Shop Popup big progress, already showing properly but no text and key yet

--- Source/BowSurvivors/Characters/BWSShopCharacter.cpp
+++ Source/BowSurvivors/Characters/BWSShopCharacter.cpp
@@ -3,9 +3,54 @@
 
 #include "Characters/BWSShopCharacter.h"
 #include "Components/SphereComponent.h"
+#include "Characters/BWSPlayerCharacter.h"
+#include "Components/BillboardComponent.h"
 
 
 ABWSShopCharacter::ABWSShopCharacter()
 {
-    ShopAccessPopupRadius = CreateDefaultSubobject<USphereComponent>(TEXT("ShopAccessPopupRadius"));
+    InitializeComponents();
 }
+
+void ABWSShopCharacter::BeginPlay()
+{
+    Super::BeginPlay();
+
+    BindOnOverlapCallbacks();
+}
+
+void ABWSShopCharacter::InitializeComponents()
+{
+    ShopAccessSphere = CreateDefaultSubobject<USphereComponent>(TEXT("ShopAccessPopupRadius"));
+    ShopAccessSphere->SetupAttachment(GetRootComponent());
+    ShopAccessSphere->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+    ShopAccessSphere->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+
+    ShopPopupBillboard = CreateDefaultSubobject<UBillboardComponent>(TEXT("ShopPopupBillboard"));
+    ShopPopupBillboard->SetupAttachment(GetRootComponent());
+    ShopPopupBillboard->SetVisibility(false);
+    ShopPopupBillboard->SetHiddenInGame(false);
+}
+
+void ABWSShopCharacter::BindOnOverlapCallbacks()
+{
+    ShopAccessSphere->OnComponentBeginOverlap.AddDynamic(this, &ABWSShopCharacter::OnPlayerEnteredShopRadius);
+    ShopAccessSphere->OnComponentEndOverlap.AddDynamic(this, &ABWSShopCharacter::OnPlayerLeftShopRadius);
+}
+
+void ABWSShopCharacter::OnPlayerEnteredShopRadius(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    ABWSPlayerCharacter* const EnteredCharacter = Cast<ABWSPlayerCharacter>(OtherActor);
+    if (!EnteredCharacter) return;
+
+    PlayerEnteredRadius(EnteredCharacter);
+}
+
+void ABWSShopCharacter::OnPlayerLeftShopRadius(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+    ABWSPlayerCharacter* const LeftCharacter = Cast<ABWSPlayerCharacter>(OtherActor);
+    if (!LeftCharacter) return;
+
+    PlayerLeftRadius(LeftCharacter);
+}
+
--- Source/BowSurvivors/Characters/BWSShopCharacter.h
+++ Source/BowSurvivors/Characters/BWSShopCharacter.h
@@ -6,6 +6,10 @@
 #include "Characters/BWSBaseCharacter.h"
 #include "BWSShopCharacter.generated.h"
 
+class USphereComponent;
+class UBillboardComponent;
+class ABWSPlayerCharacter;
+
 /**
  * 
  */
@@ -13,5 +17,35 @@ UCLASS()
 class BOWSURVIVORS_API ABWSShopCharacter : public ABWSBaseCharacter
 {
 	GENERATED_BODY()
-	
+
+public:
+    ABWSShopCharacter();
+
+    UFUNCTION(BlueprintImplementableEvent)
+    void PlayerEnteredRadius(ABWSPlayerCharacter* PlayerCharacter);
+
+    UFUNCTION(BlueprintImplementableEvent)
+    void PlayerLeftRadius(ABWSPlayerCharacter* PlayerCharacter);
+
+protected:
+    // Called when the game starts or when spawned
+    virtual void BeginPlay() override;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Components")
+    USphereComponent* ShopAccessSphere;
+
+    UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = "Components")
+    UBillboardComponent* ShopPopupBillboard;
+    
+private:
+    void InitializeComponents();
+
+    void BindOnOverlapCallbacks();
+
+    UFUNCTION()
+    void OnPlayerEnteredShopRadius(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+    UFUNCTION()
+    void OnPlayerLeftShopRadius(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
 };

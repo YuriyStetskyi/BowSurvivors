16ef1f684380592880a3b29ee0011e9affc05f22
Yuriy Stetskyi <yuriystetskyi@gmail.com>
2025-11-15 23:56:44 +0200

    Refactoring #1. Some refactoring. Stopped at BWSAbilitySystemComponent

--- Source/BowSurvivors/GameplayAbilitySystem/BWSAbilitySystemComponent.h
+++ Source/BowSurvivors/GameplayAbilitySystem/BWSAbilitySystemComponent.h
@@ -19,17 +19,22 @@ class BOWSURVIVORS_API UBWSAbilitySystemComponent : public UAbilitySystemCompone
 	GENERATED_BODY()
 	
 public:
-
+    /* Should be called after AbilityActorInfo was set. */
     void AbilityActorInfoSet();
    
+    /* Delegate that runs when you want to apply gameplay effects to self. */
     FEffectAssetTags EffectAssetTags;
 
+    /* Adds array of abilities to character that uses current ASC */
     void AddCharacterAbilities(const TArray<TSubclassOf<UGameplayAbility>>& StartupAbilities);
 
+    /* Function that runs when input tied to tag thats tied to ability is HELD */
     void AbilityInputTagHeld(const FGameplayTag& InputTag);
+    /* Function that runs when input tied to tag thats tied to ability is RELEASED */
     void AbilityInputTagReleased(const FGameplayTag& InputTag);
 
 protected:
+    /* Function that runs when gameplay effect is applied to self. */
     void OnGameplayEffectAppliedToSelf(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle);
 
 
